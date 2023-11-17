452c7b011a23f1c44c262cf8b5daaa9f3a6ac932
tropangpotchiph86 <tropangpotchiph@gmail.com>
2023-11-17 11:59:41 +0800

    03crud

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Listing;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class ListingController extends Controller
 {
@@ -25,4 +26,27 @@ class ListingController extends Controller
         ]);
     }
 
+    //Show Create Form
+    public function create() {
+        return view('listings.create');
+    }
+
+    //Store Listing Data
+    public function store(Request $request){
+        // dd($request->all());
+        $formFields = $request->validate([
+            'title' => 'required',
+            'company' => ['required', Rule::unique('listings')],
+            'location' => 'required',
+            'website' => 'required',
+            'email' => 'required',
+            'tags' => 'required',
+            'description' => 'required',
+        ]);
+
+        Listing::create($formFields);
+
+        return redirect('/')->with('message', 'Job Posting created successfully!');
+
+    }
 }
