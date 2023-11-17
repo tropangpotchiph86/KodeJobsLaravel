3d709f60f49ea521f43b5cf6066654f4dfed1fea
tropangpotchiph86 <tropangpotchiph@gmail.com>
2023-11-17 16:36:59 +0800

    08authentication

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -46,20 +46,42 @@
           class="logo"
       /></a>
       <ul class="flex space-x-6 mr-6 text-lg">
+        @auth
+        <li>
+          <span class="font-bold uppercase">WELCOME {{auth()->user()->name}}</span>
+        </li>
         <li>
           <a
-            href="register.html"
+            href="/listings/manage"
+            class="hover:text-laravel"
+            ><i class="fa-solid fa-arrow-right-to-bracket"></i> Manage Job Listings</a
+          >
+        </li>
+        <li>
+          <form class="inline" method="POST" action="/logout">
+            @csrf
+            <button type="submit" class="hover:text-laravel">
+              <i class="fa-solid fa-door-closed"></i> Logout
+            </button>
+          </form>
+
+        </li>
+        @else
+        <li>
+          <a
+            href="/register"
             class="hover:text-laravel"
             ><i class="fa-solid fa-user-plus"></i> Register</a
           >
         </li>
         <li>
           <a
-            href="login.html"
+            href="/login"
             class="hover:text-laravel"
             ><i class="fa-solid fa-arrow-right-to-bracket"></i> Login</a
           >
         </li>
+        @endauth
       </ul>
     </nav>
   
